c8c5cf2bbb59da8414f2c8c040c091291495ae0b
Nguyễn Đình Hào <dinhhaobk97@gmail.com>
2018-11-01 02:59:05 +0700

    Add sprites and file class

---
+++ Classes/Spritesheet.py
@@ -0,0 +1,22 @@
+##############################################
+#	Assignment 3 - Game Programming - HK181
+#
+#   Group 4:
+#   Pham Quang Minh - 1512016
+#   Nguyen Dinh Hao - 1510896
+#   Vu Anh Tuan - 1513888
+############################################
+
+import pygame as pg
+
+class Spritesheet:
+    # Class for loading and parsing spritesheets
+    def __init__(self, filename):
+        self.spritesheet = pg.image.load(filename).convert()
+
+    def get_image(self, x, y, width, height):
+        # Grab an image out of a larger spritesheet
+        image = pg.Surface((width, height))
+        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
+        image = pg.transform.scale(image, (width // 2, height // 2))
+        return image
